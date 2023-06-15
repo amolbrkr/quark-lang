e83a8073ab087364c59d6f7b49a680fab11a339e
Amol Borkar <amolborkar5@gmail.com>
2023-06-15 20:37:02 +0530

    added a counter

--- lexer.py
+++ lexer.py
@@ -10,9 +10,10 @@ lexer = IndentLexer(lex.lex())
 if __name__ == "__main__":
     with open(sys.argv[1], "r") as inputf:
         lexer.input(inputf.read())
-
+        count = 0
         while True:
             tok = lexer.token()
             if not tok:
                 break
-            print(tok)
+            print(f"#{count}   {tok}")
+            count += 1

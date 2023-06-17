2670c79dee37f80cb20656e3f65bb520d915255f
Amol Borkar <amolborkar5@gmail.com>
2023-06-17 18:46:47 +0530

    minor changes

--- quark_parser.py
+++ quark_parser.py
@@ -3,16 +3,42 @@ from quark_lexer import QuarkLexer
 from dataclasses import dataclass
 
 
+(
+    CompilationUnit,
+    Statement,
+    Expression,
+    Function,
+    Argument,
+    Identifier,
+    Literal,
+    Operator,
+) = range(7)
+
+
 @dataclass
 class TreeNode:
+    type: int
     t: Token
     left: any = None
     right: any = None
 
 
-@dataclass
 class QuarkParser:
-    lexer: QuarkLexer
+    def __init__(self, lexer):
+        self.tree = None
+        self.lexer = lexer
+        self.tokens = self.lexer.token_stream
+    
+    def token(self):
+        return next(self.tokens)
 
-    def parse(self):
+    def parse_statement(self):
+        t = self.token()
+        if t.type == 'ID':
+            n1 = TreeNode(Identifier, t)
+            n = self.token()
+            if n.type == 'EQUALS':
+                n2 = TreeNode(Operator, n, left=n1)
+    
+    def parse_function(self):
         pass

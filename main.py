f7b1510f50bf1452ffe4a97a8b4fb5d6e8a06685
Amol Borkar <amolborkar5@gmail.com>
2023-06-17 18:46:32 +0530

    minor refactor

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import sys
 import ply.lex as lex
 from lex_grammar import *
 from quark_lexer import QuarkLexer
-from quark_parser import TreeNode
 
 # Lexer
 lexer = QuarkLexer(lex.lex())
@@ -11,10 +10,6 @@ lexer = QuarkLexer(lex.lex())
 if __name__ == "__main__":
     with open(sys.argv[1], "r") as inputf:
         lexer.input(inputf.read())
-        count = 0
-        while True:
-            tok = lexer.token()
-            if not tok:
-                break
-            print(f"#{count}   {tok}")
-            count += 1
+        tokens = lexer.token_stream
+        for i, tok in enumerate(tokens):
+            print(i, tok)

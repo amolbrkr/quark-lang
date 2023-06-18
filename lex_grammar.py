3dd72ffaced5552303659abd591154f161853f9d
Amol Borkar <amolborkar5@gmail.com>
2023-06-18 15:49:48 +0530

    Fixed lexer not capturing Floats

--- lex_grammar.py
+++ lex_grammar.py
@@ -91,15 +91,15 @@ def t_ID(t):
 t_STR = r'"([^"\n]|(\\"))*"'
 
 
-def t_INT(t):
-    r"\d+"
-    t.value = int(t.value)
-    return t
-
-
 def t_FLOAT(t):
     r"(\d*\.\d+)|(\d+\.\d*)"
     t.value = float(t.value)
+    return t
+
+
+def t_INT(t):
+    r"\d+"
+    t.value = int(t.value)
     return t
 
 

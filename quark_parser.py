2d4ae4b09668b79f32348b085f02e5464887209d
Amol Borkar <amolborkar5@gmail.com>
2023-06-19 20:02:29 +0530

    Updated treenode, re-wrote parse_stat and added parse_bloc

--- quark_parser.py
+++ quark_parser.py
@@ -1,6 +1,6 @@
 from enum import Enum
 from ply.lex import Token
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
 class NodeType(Enum):
@@ -20,13 +20,11 @@ class NodeType(Enum):
 @dataclass
 class TreeNode:
     type: NodeType
-    t: Token
-    left: any = None
-    mid: any = None
-    right: any = None
+    tok: Token
+    children: list = field(default_factory=list)
 
     def __str__(self):
-        return f"{self.type}[{self.t.type}]"
+        return f"{self.type}[{self.tok.type}]"
 
 
 class QuarkParser:
@@ -38,53 +36,49 @@ class QuarkParser:
     def _is_term(self):
         return self.tok.type in ["ID", "INT", "FLOAT", "STR"]
 
-    def next_token(self):
+    def _next(self):
         self.tok = self.lexer.token()
 
-    def error(self, msg):
+    def _error(self, msg):
         raise Exception(f"ParseError: {msg}")
 
-    def parse_stat(self):
-        print(f"parse_statement: {self.tok}")
+    def _parse_bloc(self):
+        print(f"parse_bloc: {self.tok}")
         n = None
 
-        if self._is_term():
-            id = self.parse_term()
-            self.next_token()
-            if self.tok.type == "EQUALS":
-                n = TreeNode(NodeType.Assignment, self.tok, left=id)
-                self.next_token()
-                n.mid = self.parse_expr()
-        elif self.tok.type == "IF":
-            n = TreeNode(NodeType.Statement, self.tok)
-            self.next_token()
-            n.left = self.parse_expr()
-            self.next_token()
-            if self.tok.type == "COLON":
-                self.next_token()
-                while self.tok.type in ["NEWLINE", "INDENT"]:
-                    self.next_token()
-                n.mid = self.parse_stat()
-                self.next_token()
-                while self.tok.type in ["NEWLINE", "DEDENT"]:
-                    self.next_token()
-                print("before else", self.tok)
-                if self.tok.type == "ELSE":
-                    print("else start")
-                    self.next_token()
-                    self.next_token()
-                    while self.tok.type in ["NEWLINE", "INDENT"]:
-                        self.next_token()
-                    n.right = self.parse_stat()
-        elif self.tok.type == "FN":
-            n = TreeNode(NodeType.Function, self.tok, left=self.parse_func())
+        while self.tok.type in ["NEWLINE", "INDENT"]:
+            self._next()
         else:
-            n = TreeNode(NodeType.Expression, self.tok, left=self.parse_expr())
+            n = self._parse_stat()
+            self._next()
 
-        print(f"Node: {n}")
+            while self.tok.type in ["NEWLINE", "DEDENT"]:
+                self._next()
+
         return n
 
-    def parse_term(self):
+    def _parse_stat(self):
+        print(f"parse_stat: {self.tok}")
+        n = None
+
+        if self.tok.type == "ID":
+            n = TreeNode(NodeType.Identifier, self.tok)
+            self._next()
+            if self.tok.type == "=":
+                self._next()
+                n.children.append(self._parse_expr())
+        elif self.tok.type == "if":
+            pass
+        elif self.tok.type == "fn":
+            pass
+        elif self._is_term():
+            n = self._parse_expr()
+        else:
+            self._error(f"Unexpected token: {self.tok.value}")
+
+        return n
+
+    def _parse_term(self):
         if not self._is_term():
             self.error(f"Expected Identifier or Literal got '{self.tok.value}'")
 
@@ -92,13 +86,13 @@ class QuarkParser:
             NodeType.Identifier if self.tok.type == "ID" else NodeType.Literal, self.tok
         )
 
-    def parse_expr(self):
+    def _parse_expr(self):
         print(f"parse_expr: {self.tok}")
         n = None
 
         if self._is_term():
             lterm = self.parse_term()
-            self.next_token()
+            self._next()
             if self.tok.type in [
                 "PLUS",
                 "MINUS",
@@ -111,29 +105,31 @@ class QuarkParser:
                 "DEQ",
                 "NE",
             ]:
-                n = TreeNode(NodeType.Operator, self.tok, left=lterm)
-                self.next_token()
-                n.mid = self.parse_term()
+                n = TreeNode(NodeType.Operator, self.tok)
+                n.children.append(lterm)
+                self._next()
+                n.children.append(self._parse_term())
             else:
                 n = lterm
         elif self.tok.type == "LPAR":
-            self.next_token()
+            self._next()
             self.parse_expr()
         else:
             self.error(f"Expected Term or '(' but got '{self.tok.value}'")
 
         return n
 
-    def parse_asgn(self):
+    def _parse_asgn(self):
         print(f"parse_asgn: {self.tok}")
 
-    def parse_func(self):
+    def _parse_func(self):
         print(f"parse_func: {self.tok}")
 
     def parse(self):
-        self.next_token()
-        self.tree = TreeNode(NodeType.CompilationUnit, self.tok, left=self.parse_stat())
+        self._next()
+        self.tree = TreeNode(NodeType.CompilationUnit, self.tok)
+        self.tree.children.append(self._parse_bloc())
 
-        self.next_token()
-        # if self.tok.type != "EOF":
-        #     self.error(f"Expected EOF but got {self.tok.value}")
+        self._next()
+        if self.tok.type != "EOF":
+            self._error(f"Expected EOF but got {self.tok.value}")

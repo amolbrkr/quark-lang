5ceed998cd772aa0a1629c93dcdfdc34576a63f6
Amol Borkar <amolborkar5@gmail.com>
2023-06-25 16:16:53 +0530

    Pratt parser impl

---
+++ expr_parser.py
@@ -0,0 +1,55 @@
+from helper_types import *
+
+
+class ExprParser:
+    def __init__(self, parser):
+        self.parser = parser
+        self.rules = [
+            {"tok": "PLUS", "rule": Rule(Precedence.Term, infix=self.binary)},
+            {"tok": "NE", "rule": Rule(Precedence.Zero, prefix=self.unary)},
+            {"tok": "INT", "rule": Rule(Precedence.Zero, prefix=self.number)},
+            {"tok": "MULTIPLY", "rule": Rule(Precedence.Factor, infix=self.binary)},
+            {"tok": "DIVIDE", "rule": Rule(Precedence.Factor, infix=self.binary)},
+            {"tok": "ID", "rule": Rule(Precedence.Zero, self.identifier)},
+            {
+                "tok": "MINUS",
+                "rule": Rule(Precedence.Term, prefix=self.unary, infix=self.binary),
+            },
+        ]
+
+    def rule(self, tok_type):
+        print(f"Token type: {tok_type}")
+        return next(filter(lambda x: x["tok"] == tok_type, self.rules))["rule"]
+
+    def identifier(self):
+        return TreeNode(NodeType.Identifier, self.parser.cur())
+
+    def number(self):
+        return TreeNode(NodeType.Literal, self.parser.cur())
+
+    def unary(self):
+        node = TreeNode(NodeType.Operator, self.parser.cur())
+        node.children.append(self.parse(Precedence.Unary))
+        return node
+
+    def binary(self, left):
+        node = TreeNode(NodeType.Operator, self.parser.cur())
+        rule = self.rule(self.parser.cur().type)
+        node.children.extend([left, self.parse(rule.precedence + 1)])
+        return node
+
+    def parse(self, precedence=Precedence.Term):
+        tok = self.parser.consume()
+        prefix = self.rule(tok.type).prefix
+
+        if not prefix:
+            raise Exception("Expected expression.")
+
+        expr = prefix
+
+        while self.rule(self.parser.peek().type).precedence >= precedence:
+            tok = self.parser.consume()
+            infix = self.rule(tok.type).infix
+            expr = infix(expr)
+
+        return expr

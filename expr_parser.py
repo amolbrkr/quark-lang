348995c5697fa5f6316cbba64acf8f06d5b570d4
Amol Borkar <amolborkar5@gmail.com>
2023-06-25 23:28:40 +0530

    added tok type to Rule class and fixed a bug where precedence was not working

--- expr_parser.py
+++ expr_parser.py
@@ -5,21 +5,19 @@ class ExprParser:
     def __init__(self, parser):
         self.parser = parser
         self.rules = [
-            {"tok": "PLUS", "rule": Rule(Precedence.Term, infix=self.binary)},
-            {"tok": "NE", "rule": Rule(Precedence.Zero, prefix=self.unary)},
-            {"tok": "INT", "rule": Rule(Precedence.Zero, prefix=self.number)},
-            {"tok": "MULTIPLY", "rule": Rule(Precedence.Factor, infix=self.binary)},
-            {"tok": "DIVIDE", "rule": Rule(Precedence.Factor, infix=self.binary)},
-            {"tok": "ID", "rule": Rule(Precedence.Zero, prefix=self.identifier)},
-            {
-                "tok": "MINUS",
-                "rule": Rule(Precedence.Term, prefix=self.unary, infix=self.binary),
-            },
+            Rule("PLUS", Precedence.Term, infix=self.binary),
+            Rule("MINUS", Precedence.Term, prefix=self.unary, infix=self.binary),
+            Rule("MULTIPLY", Precedence.Factor, infix=self.binary),
+            Rule("DIVIDE", Precedence.Factor, infix=self.binary),
+            Rule("EQUALS", Precedence.Assignment, infix=self.binary),
+            Rule("NE", Precedence.Zero, prefix=self.unary),
+            Rule("INT", Precedence.Zero, prefix=self.number),
+            Rule("FLOAT", Precedence.Zero, prefix=self.number),
+            Rule("ID", Precedence.Zero, prefix=self.identifier),
         ]
 
     def rule(self, tok_type):
-        print(f"Token type: {tok_type}")
-        return next(filter(lambda x: x["tok"] == tok_type, self.rules))["rule"]
+        return next(filter(lambda x: x.type == tok_type, self.rules))
 
     def identifier(self):
         return TreeNode(NodeType.Identifier, self.parser.prev)
@@ -29,18 +27,17 @@ class ExprParser:
 
     def unary(self):
         node = TreeNode(NodeType.Operator, self.parser.prev)
-        node.children.append(self.parse(Precedence.Unary))
+        node.children.append(self.parse(precedence=Precedence.Unary))
         return node
 
     def binary(self, left):
         node = TreeNode(NodeType.Operator, self.parser.prev)
-        rule = self.rule(self.parser.cur.type)
-        node.children.extend([left, self.parse(rule.precedence + 1)])
+        rule = self.rule(node.tok.type)
+        node.children.extend([left, self.parse(precedence=rule.precedence + 1)])
         return node
 
-    def parse(self, precedence=Precedence.Term):
-        tok = self.parser.consume()
-        prefix = self.rule(tok.type).prefix
+    def parse(self, precedence=Precedence.Assignment):
+        prefix = self.rule(self.parser.consume().type).prefix
 
         if not prefix:
             raise Exception("Expected expression.")
@@ -48,11 +45,10 @@ class ExprParser:
         expr = prefix()
 
         while (
-            self.parser.cur.type not in ["NEWLINE", "COMMA", "COLON"]
+            self.parser.cur.type not in ["NEWLINE", "COMMA", "COLON", "RPAR"]
             and self.rule(self.parser.cur.type).precedence >= precedence
         ):
-            tok = self.parser.consume()
-            infix = self.rule(tok.type).infix
+            infix = self.rule(self.parser.consume().type).infix
             expr = infix(expr)
 
         return expr

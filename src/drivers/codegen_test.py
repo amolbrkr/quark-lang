5d4607b65a04a8cb731c78139f4518732e29690f
Amol Borkar <amolborkar5@gmail.com>
2023-07-10 10:29:09 +0530

    updated code to call C code using ctypes

--- src/drivers/codegen_test.py
+++ src/drivers/codegen_test.py
@@ -1,25 +1,37 @@
+import os
 import sys
+import ctypes
 import ply.lex as lex
-from utils import treeviz
+from utils import genctree
 from core.lex_grammar import *
+from core.helper_types import TreeNode
 from core.quark_lexer import QuarkLexer
 from core.quark_parser import QuarkParser
-from core.codegen import QuarkCodeGen
 
-
-qc = QuarkCodeGen()
 
 # Lexer
 lexer = QuarkLexer(lex.lex())
+
+# C++ Library for codegen
+lib_codegen = ctypes.cdll.LoadLibrary(
+    os.path.abspath("../src/core/codegen/build/libquark-codegen.so")
+)
+
+# lib_codegen.consumeTree.argtypes = [ctypes.POINTER(TreeNode)]
+# lib_codegen.consumeTree.restype = None
 
 if __name__ == "__main__":
     with open(sys.argv[1], "r") as inputf:
         lexer.input(inputf.read())
         parser = QuarkParser(lexer.token_stream)
-
         parser.parse()
-        viz = treeviz.TreeViz()
+
         if parser.tree:
             print("Codegen begins\n")
-            qc.generate(parser.tree)
-            print(qc.cur_module)
+
+            genctree.gen_c_tree(parser.tree)
+            # lib_codegen.consumeTree(ctypes.byref(parser.tree))
+
+            # proto = ctypes.CFUNCTYPE(ctypes.c_void_p, ctypes.py_object)
+            # consume_tree = proto(("consumeTree", lib_codegen))
+            # consume_tree(parser.tree)

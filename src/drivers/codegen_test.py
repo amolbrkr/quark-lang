c450d4bc835f8fa76cbc46ca95acb10e4a27f1f8
Amol Borkar <amolborkar5@gmail.com>
2023-07-11 02:20:46 +0530

    alot of changes and test

--- src/drivers/codegen_test.py
+++ src/drivers/codegen_test.py
@@ -4,21 +4,19 @@ import ctypes
 import ply.lex as lex
 from utils import genctree
 from core.lex_grammar import *
-from core.helper_types import TreeNode
+from core.helper_types import *
 from core.quark_lexer import QuarkLexer
 from core.quark_parser import QuarkParser
-
+import c_codegen
 
 # Lexer
 lexer = QuarkLexer(lex.lex())
 
 # C++ Library for codegen
-lib_codegen = ctypes.cdll.LoadLibrary(
-    os.path.abspath("../src/core/codegen/build/libquark-codegen.so")
-)
+# lib_codegen = ctypes.cdll.LoadLibrary(
+#     os.path.abspath("../src/core/codegen/build/libquark-codegen.so")
+# )
 
-# lib_codegen.consumeTree.argtypes = [ctypes.POINTER(TreeNode)]
-# lib_codegen.consumeTree.restype = None
 
 if __name__ == "__main__":
     with open(sys.argv[1], "r") as inputf:
@@ -27,10 +25,15 @@ if __name__ == "__main__":
         parser.parse()
 
         if parser.tree:
-            print("Codegen begins\n")
-
-            genctree.gen_c_tree(parser.tree)
-            # lib_codegen.consumeTree(ctypes.byref(parser.tree))
+            print("Codegen begins\n") 
+            c_codegen.objTest(parser.tree)
+            # c_codegen.processTree(parser.tree)
+            # lib_codegen.consumeTree.argtypes = [ctypes.py_object]
+            # lib_codegen.consumeTree.argtypes = [
+            #     cnode_factory(len(parser.tree.children))
+            # ]
+            # lib_codegen.consumeTree.restype = None
+            # lib_codegen.consumeTree(genctree.gen_c_tree(parser.tree)())
 
             # proto = ctypes.CFUNCTYPE(ctypes.c_void_p, ctypes.py_object)
             # consume_tree = proto(("consumeTree", lib_codegen))

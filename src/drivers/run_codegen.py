09572d38373c8f88f45adc06e6cbd2cc13495323
Amol Borkar <amolborkar5@gmail.com>
2023-07-15 14:14:06 +0530

    removed junk

--- src/drivers/run_codegen.py
+++ src/drivers/run_codegen.py
@@ -1,22 +1,12 @@
-import os
 import sys
-import ctypes
 import ply.lex as lex
-from utils import genctree
 from core.lex_grammar import *
 from core.helper_types import *
 from core.quark_lexer import QuarkLexer
 from core.quark_parser import QuarkParser
 import quark_codegen as cg
 
-# Lexer
 lexer = QuarkLexer(lex.lex())
-
-# C++ Library for codegen
-# lib_codegen = ctypes.cdll.LoadLibrary(
-#     os.path.abspath("../src/core/codegen/build/libquark-codegen.so")
-# )
-
 
 if __name__ == "__main__":
     with open(sys.argv[1], "r") as inputf:
